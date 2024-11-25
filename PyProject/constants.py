065aebe8b13c6e5ecf096d32bb6482abfbcd2aed
Владислав Павлов <pavlov.viu@phystech.edu>
2024-11-26 01:10:25 +0300

    Добавил отдельный файл с константами

---
+++ PyProject/constants.py
@@ -0,0 +1,16 @@
+# Константы экрана
+WIDTH = 1470
+HEIGHT = 890
+FPS = 300
+
+# Константы мяча
+BALL_RADIUS = 10
+SPEED = 10
+
+# Цвета
+BACKGROUND_COLOR = (224, 235, 235)
+BALL_COLOR = (60, 60, 60)
+RECT_COLOR = (255, 255, 255)
+
+# Другие константы
+COLLISION_OFFSET = 10
